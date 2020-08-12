3e354f2b82739901051fba90bd0ddd6b7e02f72d
UdaraWanasinghe <udara.developer@gmail.com>
2020-08-12 17:09:24 +0530

    filter change

--- track.sh
+++ track.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+#net 103.2.152.0/22 or net 74.125.0.0/16 or net 172.217.0.0/16
 while [ true ]
 do
-sudo iftop -i wlp1s0 -f "net 103.2.152.0/22 or net 74.125.0.0/16 or net 172.217.0.0/16 or net 172.217.0.0/16" -t -s 300 > "/home/udara/Documents/data_track/report/$(date +"%Y-%m-%d %T").txt"
+sudo iftop -i wlp1s0 -f "net 103.2.152.0/22 or net 74.125.0.0/16 or net 172.217.0.0/16" -t -s 300 > "/home/udara/Documents/data_track/report/$(date +"%Y-%m-%d %T").txt"
 done
