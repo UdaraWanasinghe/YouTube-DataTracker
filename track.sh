46c452c508a0ceb6f01b0c594454d750bbc128b8
UdaraWanasinghe <udara.developer@gmail.com>
2020-08-13 14:57:13 +0530

    add ipv6 addresses

--- track.sh
+++ track.sh
@@ -2,5 +2,5 @@
 #net 103.2.152.0/22 or net 74.125.0.0/16 or net 172.217.0.0/16
 while [ true ]
 do
-sudo iftop -i wlp1s0 -f "net 103.2.152.0/22 or net 74.125.0.0/16 or net 172.217.0.0/16" -t -s 300 > "/home/udara/Documents/data_track/report/$(date +"%Y-%m-%d %T").txt"
+sudo iftop -i wlp1s0 -f "net 103.2.152.0/22 or net 74.125.0.0/16 or net 172.217.0.0/16 or net 2404:c000::/32 or net 2404:6800::/32" -t -s 300 > "/home/udara/Documents/data_track/report/$(date +"%Y-%m-%d %T").txt"
 done
